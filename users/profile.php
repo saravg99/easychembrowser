59a82c711672696394c262f38b054849236cf33a
mariaartlle <mariaartlle@gmail.com>
2022-03-08 19:17:12 +0100

    profile.php link fixed

--- users/profile.php
+++ users/profile.php
@@ -62,23 +62,23 @@ $rs = mysqli_query($link, $sql) or print mysqli_error($link);
 
       <nav id="navbar" class="navbar">
         <ul>
-               
+
          <?php if ($_SESSION["loggedin"] == true) {?>
                       <?= '<li style="color:white"><a><b>Welcome '. $_SESSION['username'] .'!</b></a><li><a class="" href="../index.php">Search page</a></li></li><li><a href="profile.php">My favourites</a></li><li><a href="logout.php">Log Out</a></li>'; ?>
                   <?php    } else {?>
                       <?= '<li><a class="" href="../index.php">Search page</a></li><li><a href="register.php">Sign Up</a></li>
           <li><a href="login.php">Login</a></li>';?>
                   <?php } ?>
-          
-          
-          
+
+
+
         </ul>
         <i class="bi bi-list mobile-nav-toggle"></i>
       </nav><!-- .navbar -->
 
     </div>
   </header><!-- End Header -->
-  
+
 
 
 <br></br>
@@ -100,7 +100,7 @@ $rs = mysqli_query($link, $sql) or print mysqli_error($link);
       <tbody>
           <?php while ($rsF = mysqli_fetch_assoc($rs)) { ?>
           <tr>
-              <td><a href="single_search.php?cid=<?= $rsF['CID'] ?>"><?= $rsF['CID'] ?></a></td>
+              <td><a href="../single_search.php?cid=<?= $rsF['CID'] ?>"><?= $rsF['CID'] ?></a></td>
               <td>
 
               	 <?php if ($rsF['header']) {?>
